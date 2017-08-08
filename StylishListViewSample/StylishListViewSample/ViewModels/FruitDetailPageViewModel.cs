b18c0c38d2b056058569b80496bf82ba69c80e29
nuits.jp <nuits.jp@live.jp>
2017-08-08 16:34:37 +0900

    Update Samples.

--- StylishListViewSample/StylishListViewSample/ViewModels/FruitDetailPageViewModel.cs
+++ StylishListViewSample/StylishListViewSample/ViewModels/FruitDetailPageViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using Kamishibai.Xamarin.Forms;
+using StylishListViewSample.Models;
 
 namespace StylishListViewSample.ViewModels
 {
-    public class FruitDetailPageViewModel : ViewModelBase, IPageInitializeAware<Fruit>
+
+    /// <summary>
+    /// When receiving Fruit object directly.
+    /// </summary>
+    // public class FruitDetailPageViewModel : ViewModelBase, IPageInitializeAware<Fruit>
+
+    /// <summary>
+    /// When receiving Fruit's id.
+    /// </summary>
+    public class FruitDetailPageViewModel : ViewModelBase, IPageInitializeAware<int>
     {
-        public void OnInitialize(Fruit fruit) => Fruit = fruit;
+        /// <summary>
+        /// When receiving Fruit object directly.
+        /// </summary>
+        // public void OnInitialize(Fruit fruit) => Fruit = fruit;
+
+        /// <summary>
+        /// When receiving Fruit's id.
+        /// </summary>
+        public void OnInitialize(int id) => Fruit = FruitsRepository.FindById(id);
 
         private Fruit _fruit;
         public Fruit Fruit
--- StylishListViewSample/StylishListViewSample/ViewModels/FruitsListPageViewModel.cs
+++ StylishListViewSample/StylishListViewSample/ViewModels/FruitsListPageViewModel.cs
@@ -2,26 +2,23 @@
 using System.Collections.Generic;
 using Kamishibai.Xamarin.Forms;
 using Kamishibai.Xamarin.Forms.Mvvm;
+using StylishListViewSample.Models;
 using Xamarin.Forms;
 
 namespace StylishListViewSample.ViewModels
 {
     public class FruitsListPageViewModel : ViewModelBase
     {
-        public IList<Fruit> Fruits { get; } = new List<Fruit>();
+        public IReadOnlyList<Fruit> Fruits { get; } = FruitsRepository.Fruits;
 
-        public NavigationRequestCommand<Fruit> RequestDetail { get; } = new NavigationRequestCommand<Fruit>();
+        /// <summary>
+        /// When passing a Fruit object directly.
+        /// </summary>
+        // public NavigationRequestCommand<Fruit> RequestDetail { get; } = new NavigationRequestCommand<Fruit>();
 
-        public FruitsListPageViewModel()
-        {
-            Fruits.Add(new Fruit { Name = "Apple", Color = Color.Red });
-            Fruits.Add(new Fruit { Name = "Orange", Color = Color.Orange });
-            Fruits.Add(new Fruit { Name = "Pineapple", Color = Color.Orange });
-            Fruits.Add(new Fruit { Name = "Banana", Color = Color.Goldenrod });
-            Fruits.Add(new Fruit { Name = "Peach", Color = Color.DeepPink });
-            Fruits.Add(new Fruit { Name = "Mango", Color = Color.Goldenrod });
-            Fruits.Add(new Fruit { Name = "Melon", Color = Color.Green });
-            Fruits.Add(new Fruit { Name = "Grape", Color = Color.Purple });
-            Fruits.Add(new Fruit { Name = "Strawberry", Color = Color.Red });
-        }
-    }}
+        /// <summary>
+        /// When passing a Fruit's id.
+        /// </summary>
+        public NavigationRequestCommand<int> RequestDetail { get; } = new NavigationRequestCommand<int>();
+    }
+}
